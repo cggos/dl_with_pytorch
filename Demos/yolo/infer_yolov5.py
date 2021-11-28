6cc8d8de88af90749695dc37e44f9fd3c129dacb
cggos <cggos@outlook.com>
2021-11-28 14:21:03 +0800

    add yolo infer

---
+++ Demos/yolo/infer_yolov5.py
@@ -0,0 +1,12 @@
+import torch
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+# Image
+img = 'https://ultralytics.com/images/zidane.jpg'
+
+# Inference
+results = model(img)
+
+results.pandas().xyxy[0]
