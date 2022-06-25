51bba229e265fefd6bebf42c85cb67822c2a61a7
cggos <cggos@outlook.com>
2022-06-25 16:42:22 +0800

    update README.md

--- Demos/cpp/demo00/demo00.cpp
+++ Demos/cpp/demo00/demo00.cpp
@@ -1,7 +1,10 @@
 #include <torch/torch.h>
+
 #include <iostream>
 
 int main() {
+  std::cout << "cuda is available: " << torch::cuda::is_available() << std::endl;
+
   torch::Tensor tensor = torch::rand({2, 3});
   std::cout << tensor << std::endl;
 }
