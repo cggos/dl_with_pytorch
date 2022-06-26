3d89a192ef8e9ee6135abd7c95bc21ffe688b08c
cggos <cggos@outlook.com>
2022-06-26 18:09:03 +0800

    cpp demo file code format

--- Demos/cpp/demo01.cpp
+++ Demos/cpp/demo01.cpp
@@ -4,11 +4,6 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  std::cout << "cuda is available: " << torch::cuda::is_available() << std::endl;
-
-  torch::Tensor tensor = torch::rand({2, 3});
-  std::cout << tensor << std::endl;
-
   // Deserialize the ScriptModule from a file using torch::jit::load().
   std::shared_ptr<torch::jit::script::Module> module =
       std::make_shared<torch::jit::script::Module>(torch::jit::load(argv[1]));  // .pt file path
@@ -23,4 +18,6 @@ int main(int argc, char *argv[]) {
   at::Tensor output = module->forward(inputs).toTensor();
 
   std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
+
+  return 0;
 }
